29a5c74095997b3a4834f7efdf8770e46a827df7
siddardha reddy pasham <178729877+sid1114911149@users.noreply.github.com>
2026-03-03 00:13:46 +0530

    new Version

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
 // export const API_URL="http://localhost:5500";
-export const API_URL="https://swiggy-clone-backend-f4vv.onrender.com";
+export const API_URL="https://swiggy-clone-backend-2-qrqd.onrender.com";
